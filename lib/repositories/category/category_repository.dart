3295f316605692836bf58da8ae6193d197eab458
Anbesha Thapa <anbesha@exmple.com>
2022-04-05 17:18:25 +0545

    abcd

--- lib/repositories/category/category_repository.dart
+++ lib/repositories/category/category_repository.dart
@@ -5,13 +5,13 @@ import 'package:ecommerceapp/repositories/category/base_category_repository.dart
 class CategoryRepository extends BaseCategoryRepository{
   final FirebaseFirestore _firebaseFirestore;
   CategoryRepository({FirebaseFirestore? firebaseFirestore})
-  : _firebaseFirestore = _firebaseFirestore?? FirebaseFirestore.instance;
+  : _firebaseFirestore = firebaseFirestore?? FirebaseFirestore.instance;
 
 
   @override
   Stream<List<Category>> getallCategories() {
   return _firebaseFirestore.collection('categories').snapshots().map((snapshot){
-  return snapshot.docs.map((docs)=> Category.fromSnapshot(doc)).toList();
+  return snapshot.docs.map((doc)=> Category.fromSnapshot(doc)).toList();
   });
   }
 
